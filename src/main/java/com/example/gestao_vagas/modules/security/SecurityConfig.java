99168909f626248f53c9fbd35e1187bdefd06b8f
Jefferson Alves <jefff9910@outlook.com>
2024-02-11 13:08:19 -0300

    Iniciando autorização de rotas

--- src/main/java/com/example/gestao_vagas/modules/security/SecurityConfig.java
+++ src/main/java/com/example/gestao_vagas/modules/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.gestao_vagas.modules.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,9 +11,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/candidate/").permitAll()
+                            .requestMatchers("/company/").permitAll();
+                    auth.anyRequest().authenticated();
+
+                });
 
         return httpSecurity.build();
     }
