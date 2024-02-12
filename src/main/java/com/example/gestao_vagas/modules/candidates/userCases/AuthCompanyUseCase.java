285e5ca5e175dd7b0d7c634e3fb2a39fb1035efd
Jefferson Alves <jefff9910@outlook.com>
2024-02-12 18:24:36 -0300

    Criando usecase JWT

--- src/main/java/com/example/gestao_vagas/modules/candidates/userCases/AuthCompanyUseCase.java
+++ src/main/java/com/example/gestao_vagas/modules/candidates/userCases/AuthCompanyUseCase.java
@@ -1,9 +1,12 @@
 package com.example.gestao_vagas.modules.candidates.userCases;
 
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.example.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import com.example.gestao_vagas.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,13 +16,16 @@ import javax.naming.AuthenticationException;
 @Service
 public class AuthCompanyUseCase {
 
+    @Value("${security.token.secret}")
+    private String secretKey;
+
     @Autowired
     private CompanyRepository companyRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) {
+    public String execute(AuthCompanyDTO authCompanyDTO) throws Exception{
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername())
                 .orElseThrow(
                         () -> {
@@ -32,6 +38,13 @@ public class AuthCompanyUseCase {
         if(!passwordMatches) {
             throw new AuthenticationException();
         }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        var token = JWT.create().withIssuer("javagas")
+                .withSubject(company.getId().toString())
+                .sign(algorithm);
+        return token;
     }
 
 }
